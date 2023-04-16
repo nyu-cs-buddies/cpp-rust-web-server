a39c368113897de57c074bfea04fde0a3a932678
allenpthuang <allen.pt.huang@gmail.com>
2023-04-16 12:52:59 -0400

    Tidy up

--- cpp/http_server.cc
+++ cpp/http_server.cc
@@ -3,11 +3,6 @@
 #include <cstring>
 #include <vector>
 #include <filesystem>
-#include <fstream>
-#include <sstream>
-
-#include <thread>
-#include <future>
 
 #include <unistd.h>
 #include <arpa/inet.h>
@@ -15,66 +10,23 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-// Task
+#include "mime_types.h"
 // connection
 // actions: socket, bind, listen, accept, connect, recv, send, close
 // socket, bind, listen
 // accept, connect (blocking)
 // recv (blocking), send
 
-
-
-// Thread pool
-// thread picks up a task from the queue
-// thread executes the task
-
-// scheduling stuff
-// if a client has a higher response time, we can give it a lower priority
-// i.e. more connections handled by a thread. Or give this thread lower prio.
-
-// use some threads to accept connections, and the threads put immediately
-// the connection into the queue. The queue is then handled by the other
-// threads. The handling threads then use async io to recv/send data.
-// Can we do like pre-async, async, then the thread put the future into a queue
-// and the other threads can fetch it from the queue and wait for it to be ready?
-
-// caching. Multithreading gives shared memory access. We can use this to
-// do caching. Think about if we have a DELETE request and GET request.
-
 const int PORT = 8080;
-// char HTTP_OK[] = "HTTP/1.1 200 OK\r\n";
-// char HTML_CONTENT_TYPE[] = "Content-Type: text/html\r\nContent-Length: 207\r\n\r\n";
-std::string HTTP_OK = "HTTP/1.1 200 OK\r\n";
-std::string HTML_CONTENT_TYPE = "Content-Type: text/html\r\n";
-// std::string HTML_CONTENT_TYPE = "";
 
 void print_info(const std::string& str) {
   std::cerr << "\033[32;1m[INFO]\033[0m " << str << std::endl;
 }
 
-auto accept_conn(int sockfd) {
-    std::cout << "Accepting connection via accept_conn in t_accept" << std::endl;
-    struct sockaddr_in client_addr;
-    socklen_t client_len = sizeof(client_addr);
-    int newsockfd = accept(sockfd, (struct sockaddr*) &client_addr, &client_len);
-    if (newsockfd < 0) {
-        std::cerr << "Error accepting connection" << std::endl;
-        return 1;
-    }
-
-    // print the connection
-    std::cout << "Connection accepted from "
-              << inet_ntoa(client_addr.sin_addr) << ":"
-              << ntohs(client_addr.sin_port) << std::endl;
-
-    return newsockfd;
-}
-
-
 int main() {
     std::cout << "Server starting..." << std::endl;
 
-    int sockfd, newsockfd, port;
+    int sockfd, port;
     port = PORT;
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) {
@@ -94,13 +46,16 @@ int main() {
 
     // listen
     listen(sockfd, 5);
-    std::cout << "Listening on port " << port << std::endl;
+    std::cout << "Listening to "
+              << inet_ntoa(server_addr.sin_addr) << ":"
+              << ntohs(server_addr.sin_port)
+              << std::endl;
 
     // accept
-    struct sockaddr_in client_addr;
-    socklen_t client_len = sizeof(client_addr);
     while (true) {
-      newsockfd = accept(sockfd, (struct sockaddr*) &client_addr, &client_len);
+      struct sockaddr_in client_addr;
+      socklen_t client_len = sizeof(client_addr);
+      int newsockfd = accept(sockfd, (struct sockaddr*) &client_addr, &client_len);
       if (newsockfd < 0) {
           std::cerr << "Error accepting connection" << std::endl;
           return 1;
@@ -111,16 +66,17 @@ int main() {
                 << inet_ntoa(client_addr.sin_addr) << ":"
                 << ntohs(client_addr.sin_port) << std::endl;
 
-      // std::filesystem::path p{"404.html"};
       char buf[1024] = {0};
-      // memset(buf, 0, 1024);
       // recv to buf
       int n = recv(newsockfd, buf, 1023, 0);
       if (n < 0) {
           std::cerr << "Error reading from socket" << std::endl;
           return 1;
       }
-      std::cout << "Received:\n" << buf << std::endl;
+      std::cout << "=== Received HTTP request ===\n"
+                << buf
+                << "=============================\n"
+                << std::endl;
 
       auto token = strtok(buf, " \t");
       std::string method{token};
@@ -129,21 +85,37 @@ int main() {
       token = strtok(NULL, " \t\r\n");
       std::string protocol(token);
 
-      std::cout << "Method = " << method << std::endl;
-      std::cout << "URI = " << uri << std::endl;
-      std::cout << "Protocol = " << protocol << std::endl;
+      std::cout << "=== Parsed HTTP request ===" << std::endl
+                << "Method = " << method << std::endl
+                << "URI = " << uri << std::endl
+                << "Protocol = " << protocol << std::endl
+                << "===========================" << std::endl;
 
+      // send response with headers and content based on the request
       std::string file_to_serve = "404.html";
-      HTML_CONTENT_TYPE = "Content-Type: text/html\r\n";
+      std::string HTTP_STATUS_LINE = "HTTP/1.1 200 OK\r\n";
+      std::string HTML_CONTENT_TYPE = "Content-Type: text/html\r\n";
       if (uri == "/") {
         file_to_serve = "index.html";
-      } else if (uri == "/imsorry.jpg") {
-        file_to_serve = "imsorry.jpg";
-        HTML_CONTENT_TYPE = "Content-Type: image/jpeg\r\n";
       } else {
-        HTTP_OK = "HTTP/1.1 404 Not Found\r\n";
+        file_to_serve = uri.substr(1);
       }
-      // send something
+
+      // check if the file exists
+      auto filepath = std::filesystem::path(file_to_serve);
+      if (!std::filesystem::exists(filepath)) {
+        file_to_serve = "404.html";
+        HTTP_STATUS_LINE = "HTTP/1.1 404 Not Found\r\n";
+      } else {
+        // check the mime type
+        auto ext = filepath.extension();
+        if (MIME::mime_types.find(ext) != MIME::mime_types.end()) {
+          HTML_CONTENT_TYPE = "Content-Type: " + MIME::mime_types.at(ext) + "\r\n";
+        } else {
+          HTML_CONTENT_TYPE = "Content-Type: application/octet-stream\r\n";
+        }
+      }
+      // ugly way to read files at the moment
       auto file_sz = std::filesystem::file_size(file_to_serve);
       FILE* fp = fopen(file_to_serve.c_str(), "rb");
       unsigned char* send_buf = new unsigned char[file_sz + 1];
@@ -154,44 +126,23 @@ int main() {
           << file_to_serve << "!" << std::endl;
         std::cerr << "file_sz = " << file_sz << "; n_fread = " << n_fread << std::endl;
       }
-      // std::ifstream t(file_to_serve);
-      // std::stringstream buffer;
-      // buffer << t.rdbuf();
+
+      // construct the headers
+      std::string resp_hdrs = 
+        HTTP_STATUS_LINE
+        + HTML_CONTENT_TYPE
+        + "Content-Length: "
+        + std::to_string(file_sz)
+        + "\r\n\r\n";
+
+      // send it over the socket
       ssize_t n_send = 0;
-
-
-      // n_send = send(newsockfd, HTTP_OK, sizeof(HTTP_OK), 0);
-      // print_info("n_send = " + std::to_string(n_send));
-      // n_send = send(newsockfd, HTML_CONTENT_TYPE, sizeof(HTML_CONTENT_TYPE), 0);
-      // print_info("n_send = " + std::to_string(n_send));
-      // if (n_send == -1) print_info("Something's wrong!!");
-      // send(newsockfd, "\r\n", 2, 0);
-      // std::string content_length = "Content-Length: " + std::to_string(std::filesystem::file_size(p)) + "\r\n\r\n";
-      // send(newsockfd, content_length.c_str(), content_length.size(), 0);
-      // size_t nread;
-      // while ((nread = fread(buf, 1, sizeof(buf), fp)) > 0) {
-          // send(newsockfd, buf, nread, 0);
-      // }
-
-
-
-      // std::cout << buffer.str() << std::endl;
-      std::string resp_hdrs = HTTP_OK + HTML_CONTENT_TYPE + "Content-Length: "
-        + std::to_string(file_sz) + "\r\n\r\n";
-      // n_send = send(newsockfd, buffer.str().c_str(), buffer.str().size() + 1, 0);
-
       n_send = send(newsockfd, resp_hdrs.c_str(), resp_hdrs.size(), 0);
-      print_info("n_send = " + std::to_string(n_send));
-      // sendfile(newsockfd, fp_op, NULL, std::filesystem::file_size(p));
-      // send(newsockfd, "\r\n\r\n", 4, 0);
+      print_info("n_send (headers) = " + std::to_string(n_send));
 
       ssize_t rem_file_sz = file_sz;
-      //size_t block_sz = 4096;
       auto send_buf_ptr = send_buf;
       while (rem_file_sz > 0) {
-        // if (rem_file_sz < block_sz) {
-        //   block_sz = rem_file_sz;
-        // }
         n_send = send(newsockfd, send_buf_ptr, rem_file_sz, 0);
         if (n_send == -1) {
           print_info("Error on send()!");
@@ -200,7 +151,7 @@ int main() {
         send_buf_ptr += n_send;
         rem_file_sz -= n_send;
       }
-      print_info("n_send = " + std::to_string(n_send));
+      print_info("n_send (content) = " + std::to_string(n_send));
       delete[] send_buf;
       close(newsockfd);
       fclose(fp);
@@ -208,22 +159,3 @@ int main() {
     close(sockfd);
     return 0;
 }
-
-
-    // accept by thread
-    // std::thread t_accept(&accept_conn, sockfd);
-    
-    // accept by future
-    // auto future = std::async(std::launch::async, accept_conn, sockfd);
-
-    // std::thread t1(send, newsockfd, "Hello curl!\n", 12, 0);
-    // std::thread t1(sleep, 10);
-    // std::thread t2([]{
-    //     std::cout << "Hello from thread 2" << std::endl;
-    // });
-    // send hello
-    // newsockfd = future.get();
-    // std::cout << "We're gonna send hello here!" << std::endl;
-    // send(newsockfd, "Hello curl!\n", 12, 0);
-    // t1.join();
-    // t2.join();
