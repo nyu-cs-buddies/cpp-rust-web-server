42a1c725d7006bde1d17938995127d3e49169842
allenpthuang <allen.pt.huang@gmail.com>
2023-04-17 16:44:29 -0400

    Tidy up

--- cpp/threadpool.cc
+++ cpp/threadpool.cc
@@ -1,90 +1,68 @@
-#include <iostream>
-#include <string>
-#include <thread>
-#include <queue>
-#include <functional>
-#include <condition_variable>
-#include <mutex>
+#include "threadpool.h"
 
 // static unsigned int exec_count = 0;
 // static std::mutex exec_mutex;
 
-class ThreadPool {
- private:
-  std::vector<std::thread> threads;
-  std::queue<std::function<void()>> tasks;
-  std::mutex queue_mutex;
-  std::condition_variable condition;
-  bool stop_flag;
-  bool stopped;
-
-  void fetch_task() {
-    while (true) {
-      std::function<void()> task;
-      {
-        std::unique_lock<std::mutex> lock(queue_mutex);
-        condition.wait(lock, [this] {
-            return this->stop_flag || !this->tasks.empty();
-        });
-        if (stop_flag) {
-          return;
-        }
-        task = std::move(tasks.front());
-        tasks.pop();
+void ThreadPool::fetch_task() {
+  while (true) {
+    std::function<void()> task;
+    {
+      std::unique_lock<std::mutex> lock(queue_mutex);
+      condition.wait(lock, [this] {
+          return this->stop_flag || !this->tasks.empty();
+      });
+      if (stop_flag) {
+        return;
       }
-      task();
+      task = std::move(tasks.front());
+      tasks.pop();
     }
+    task();
   }
+}
 
- public:
-  ThreadPool() = delete;
-  ThreadPool(unsigned int num_threads) : stop_flag(false) {
-    threads.resize(num_threads);
-    for (auto& thread : threads) {
-      thread = std::move(std::thread(&ThreadPool::fetch_task, this));
-    }
+ThreadPool::ThreadPool(unsigned int num_threads) : stop_flag(false) {
+  threads.resize(num_threads);
+  for (auto& thread : threads) {
+    thread = std::move(std::thread(&ThreadPool::fetch_task, this));
   }
+}
 
-  ~ThreadPool() {
-    if (! stopped) {
-      stop();
-    }
+ThreadPool::~ThreadPool() {
+  if (! stopped) {
+    stop();
   }
+}
 
-  void add_task(const std::function<void()>& task) {
-    {
-      std::unique_lock<std::mutex> lock(queue_mutex);
-      tasks.push(task);
-    }
-    condition.notify_one();
-  };
-
-  void stop() {
-     // std::condition_variable cv;
-    // std::unique_lock<std::mutex> lock(busy_thread_count_mutex);
-    // cv.wait(lock, [this] {
-    //     return (this->busy_thread_count == 0) && this->tasks.empty();
-    // });
-    
-    // TODO(allenpthuang): naive workaround; might need to use condition variable.
-    while (tasks.size() > 0) {
-      std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    }
-    {
-      std::unique_lock<std::mutex> lock(queue_mutex);
-      stop_flag = true;
-    }
-    condition.notify_all();
-    for (auto& thread : threads) {
-      thread.join();
-    }
-    stopped = true;
+void ThreadPool::add_task(const std::function<void()>& task) {
+  {
+    std::unique_lock<std::mutex> lock(queue_mutex);
+    tasks.push(task);
   }
-
-  auto size() {
-    return tasks.size();
-  }
+  condition.notify_one();
 };
+
+void ThreadPool::stop() {
+    // std::condition_variable cv;
+  // std::unique_lock<std::mutex> lock(busy_thread_count_mutex);
+  // cv.wait(lock, [this] {
+  //     return (this->busy_thread_count == 0) && this->tasks.empty();
+  // });
+  
+  // TODO(allenpthuang): naive workaround; might need to use condition variable.
+  while (tasks.size() > 0) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  }
+  {
+    std::unique_lock<std::mutex> lock(queue_mutex);
+    stop_flag = true;
+  }
+  condition.notify_all();
+  for (auto& thread : threads) {
+    thread.join();
+  }
+  stopped = true;
+}
 
 
 
@@ -96,7 +74,7 @@ void hello_world() {
   std::cout << "Hello world from " << std::this_thread::get_id() << std::endl;
 }
 
-int main() {
+void test() {
   ThreadPool pool(4);
   pool.add_task([]() {
     std::cout << "Hello, world!" << std::endl;
@@ -117,5 +95,4 @@ int main() {
   //   std::unique_lock<std::mutex> lock(exec_mutex);
   //   std::cout << "exec_count for hello_world() = " << exec_count << std::endl;
   // }
-  return 0;
 }
--- cpp/threadpool.h
+++ cpp/threadpool.h
@@ -17,70 +17,14 @@ class ThreadPool {
   bool stop_flag;
   bool stopped;
 
-  void fetch_task() {
-    while (true) {
-      std::function<void()> task;
-      {
-        std::unique_lock<std::mutex> lock(queue_mutex);
-        condition.wait(lock, [this] {
-            return this->stop_flag || !this->tasks.empty();
-        });
-        if (stop_flag) {
-          return;
-        }
-        task = std::move(tasks.front());
-        tasks.pop();
-      }
-      task();
-    }
-  }
+  void fetch_task();
 
  public:
   ThreadPool() = delete;
-  ThreadPool(unsigned int num_threads) : stop_flag(false) {
-    threads.resize(num_threads);
-    for (auto& thread : threads) {
-      thread = std::move(std::thread(&ThreadPool::fetch_task, this));
-    }
-  }
+  ThreadPool(unsigned int num_threads);
+  ~ThreadPool();
 
-  ~ThreadPool() {
-    if (! stopped) {
-      stop();
-    }
-  }
-
-  void add_task(const std::function<void()>& task) {
-    {
-      std::unique_lock<std::mutex> lock(queue_mutex);
-      tasks.push(task);
-    }
-    condition.notify_one();
-  };
-
-  void stop() {
-     // std::condition_variable cv;
-    // std::unique_lock<std::mutex> lock(busy_thread_count_mutex);
-    // cv.wait(lock, [this] {
-    //     return (this->busy_thread_count == 0) && this->tasks.empty();
-    // });
-    
-    // TODO(allenpthuang): naive workaround; might need to use condition variable.
-    while (tasks.size() > 0) {
-      std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    }
-    {
-      std::unique_lock<std::mutex> lock(queue_mutex);
-      stop_flag = true;
-    }
-    condition.notify_all();
-    for (auto& thread : threads) {
-      thread.join();
-    }
-    stopped = true;
-  }
-
-  auto size() {
-    return tasks.size();
-  }
+  auto size() { return tasks.size(); }
+  void add_task(const std::function<void()>& task);
+  void stop();
 };
