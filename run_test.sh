40d76abd736cbadda63fad91700a72eb9165ce3d
allenpthuang <allen.pt.huang@gmail.com>
2023-04-18 16:18:18 -0400

    Add siege

--- run_test.sh
+++ run_test.sh
@@ -9,23 +9,34 @@ function build_cpp_multi() {
 
 bench_tool_bin=$(command -v ab)
 
-# function download_siege() {
-#   mkdir -p ext_utils && cd ext_utils
-#   wget http://download.joedog.org/siege/siege-latest.tar.gz
-#   tar -xf siege-latest.tar.gz
-# }
+function download_siege() {
+  mkdir -p ext_utils && cd ext_utils
+  wget http://download.joedog.org/siege/siege-latest.tar.gz
+  tar -xf siege-latest.tar.gz
+  cd ..
+}
 
-# # Siege benchmark utility
-# if command -v siege; then
-#   siege_bin=$(command -v siege)
-# else
-#   siege_bin=$(pwd)/ext_utils/siege-4.1.6/siege
-# fi
+function make_siege() {
+    cd ext_utils/siege-4.1.6
+    ./configure --prefix=$(pwd) && make && make install
+    cd ../../
+}
 
-# # Build if necessary
-# if [[ ! -x ./cpp/bin/http_server_multi ]]; then
-#     build_cpp_multi
-# fi
+# Siege benchmark utility
+if command -v siege; then
+  bench_tool_bin=$(command -v siege)
+elif [[ -x ./ext_utils/siege-4.1.6/bin/siege ]]; then
+  bench_tool_bin="$(pwd)/ext_utils/siege-4.1.6/bin/siege"
+else
+  download_siege
+  make_siege
+  bench_tool_bin="$(pwd)/ext_utils/siege-4.1.6/bin/siege"
+fi
+
+# Build if necessary
+if [[ ! -x ./cpp/bin/http_server_multi ]]; then
+    build_cpp_multi
+fi
 
 # Run cpp version and test
 cd cpp && module load gcc-12.2 && ./bin/http_server_multi -p 5566 -t 4 &
